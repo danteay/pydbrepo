0b1939dc38f0625e824f611da6872a9133f38bcb
Eduardo Aguilar <dante.aguilar41@gmail.com>
2021-07-29 12:57:37 -0500

    fix: try to cast even when the value has the expected value

--- pydbrepo/descriptors/field.py
+++ pydbrepo/descriptors/field.py
@@ -115,7 +115,7 @@ class Field:
         :return Any: Casted value
         """
 
-        if self._cast_to is None:
+        if self._cast_to is None or isinstance(value, self._cast_to):
             return value
 
         if self._cast_if is not None:
